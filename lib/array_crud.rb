8eeb078a741b066524be10f0a3d232f3fdb18cc2
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 00:15:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,50 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+cats = ["bean", "jean", "ham", "bobbie", "oatman"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  cat_friends = ["bean", "jean", "ham", "oatman"]
+  cat_friends << "timmy"
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  cat_friends = ["bean", "jean", "ham", "oatman"]
+  cat_friends.unshift "pearl"
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+  cat_friends = ["bean", "jean", "ham", "oatman"]
+  cat_friends.pop "oatman"
 end
 
 def remove_element_from_start_of_array(array)
-  
+  cat_friends = ["bean", "jean", "ham", "oatman"]
+  cat_friends.shift "bean"
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  cat_friends = ["bean", "jean", "ham", "oatman"]
+  cat_friends[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  cat_friends = ["bean", "jean", "ham", "oatman"]
+  cat_friends[0]
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+  cat_friends = ["bean", "jean", "ham", "oatman"]
+  cat_friends[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  cat_friends = ["bean", "jean", "ham", "oatman"]
+  cat_friends[1] "honey"
 end
