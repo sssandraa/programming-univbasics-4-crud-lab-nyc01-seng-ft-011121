a3fe978a5e4601db9ca7a994f2b2c2a9cb52d80e
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 00:37:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,6 +43,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  cat_friends = ["bean", "jean", "ham", "oatman"]
-  cat_friends[1] = "honey"
+  cat_friends = ["bean", "jean", "ham", "oatman", "totally"]
+  cat_friends[4] = "honey"
 end
