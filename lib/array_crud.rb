5ec3dcfd00a396df0a2a435eb9723c8e5e9636ce
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 00:35:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   cats = ["wow", "jean", "ham", "oatman"]
-  bean = cats.shift
+  wow = cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
