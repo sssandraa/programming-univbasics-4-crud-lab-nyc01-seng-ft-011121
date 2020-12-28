4d69b1cfc923b28151473f13d6b91410bee2358b
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 00:25:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   cat_friends = ["bean", "jean", "ham", "oatman"]
-  cat_friends[1]
+  cat_friends[2]
 end
 
 def retrieve_first_element_from_array(array)
