f9ea32d50875a77578a0fafb8ec2f988b05e682e
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 00:31:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   cats = ["bean", "jean", "ham", "arrays!"]
-  cats.pop "arrays!"
+  arrays = cats.pop
 end
 
 def remove_element_from_start_of_array(array)
