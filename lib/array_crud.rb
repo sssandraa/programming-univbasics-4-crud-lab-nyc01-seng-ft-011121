83478d4dc1f244f08811df23f2cbaf83c680db9b
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 00:35:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  cat_friends = ["bean", "jean", "ham", "oatman"]
-  cat_friends[2]
+  cats = ["bean", "jean", "ham", "oatman"]
+  cats[2]
 end
 
 def retrieve_first_element_from_array(array)
