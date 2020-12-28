cbb1f80d40de6a3b4be2129e9667533fd6d1462e
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 00:17:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-cats = ["bean", "jean", "ham", "bobbie", "oatman"]
+cats = ["bean", "jean", "ham", "oatman"]
 end
 
 def add_element_to_end_of_array(array, element)
