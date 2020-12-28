fc749a87a15db4fe236bdf1cf91f8fed4983707f
Sandra Wazaz <sandrawazaz@gmail.com>
2020-12-28 00:38:25 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,48 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+cats = ["bean", "jean", "ham", "oatman"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  cats = ["bean", "jean", "ham", "oatman"]
+  cats.push ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  cats = ["bean", "jean", "ham", "oatman"]
+  cats.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-  
+  cats = ["bean", "jean", "ham", "arrays!"]
+  arrays = cats.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  cats = ["wow", "jean", "ham", "oatman"]
+  wow = cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  cats = ["bean", "jean", "am", "oatman"]
+  cats[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  cat_friends = ["wow", "jean", "ham", "oatman"]
+  cat_friends[0]
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+  cat_friends = ["bean", "jean", "ham", "arrays!"]
+  cat_friends[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  cat_friends = ["bean", "jean", "ham", "oatman", "what"]
+  cat_friends[4] = "totally"
 end
